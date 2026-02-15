b9dfeb8517f697614e80e52b2e1c88ab9d893db0
Avi <avinashreddybapatu2006@gmail.com>
2026-02-15 23:35:11 +0530

    Update image input for saliency processing and enhance feature extraction with texture variance

--- backend/saliency_core/saliency.py
+++ backend/saliency_core/saliency.py
@@ -3,13 +3,31 @@ from skimage.color import rgb2lab
 from graph import build_adjacency, compute_weight_matrix, compute_degree_matrix, compute_normalized_similarity
 from ranking import  compute_boundary_seeds, manifold_ranking
 import numpy as np
+import cv2
 
 def compute_saliency(image, num_segments):
   lab_image = rgb2lab(image)
   # Converting image to LAB(L (Lighting), A channel(green, red), B channel(blue, yellow)) for uniform perception for human eye.)
 
   segments, num_nodes = compute_superpixels(image, num_segments)
+  gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+  gray_blur = cv2.GaussianBlur(gray, (5,5), 0)
   features = compute_features(lab_image, segments, num_nodes)
+
+  variance = np.zeros(num_nodes)
+
+  for i in range(num_nodes):
+      mask = (segments == i)
+      region_pixels = gray_blur[mask]
+      variance[i] = np.var(region_pixels)
+  variance = (variance - variance.min()) / (variance.max() - variance.min() + 1e-8)
+  variance = variance * 30
+  features = np.hstack((
+    features[:, :3],      # L, a, b
+    variance[:, None],    # texture variance
+    features[:, 3:5]      # x, y
+  ))
+
   # print(features[:, :3].max(), features[:, :3].min())
   adjacency = build_adjacency(segments, num_nodes)
 
